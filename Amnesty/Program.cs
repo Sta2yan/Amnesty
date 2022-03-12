562e6c25e4f3bf9f540f6221b83eeec1f0551148
Sta2yan <s2_stas@mail.ru>
2022-03-12 16:09:11 +0300

    Добавьте файлы проекта.

---
+++ Amnesty/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SearchCriminals
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            List<Prisoner> prisoners = new List<Prisoner>();
+            int maximumPrisoners = 30;
+            int maximumCrimes = Enum.GetNames(typeof(Crimes)).Length;
+
+            for (int i = 0; i < maximumPrisoners; i++)
+            {
+                prisoners.Add(new Prisoner($"Заключенный_{i + 1}", (Crimes)random.Next(0, maximumCrimes)));
+            }
+
+            var filteredPrisoners = prisoners.OrderBy(prisoner => prisoner.Crime).SkipWhile(prisoner => prisoner.Crime == Crimes.AntiGovernment);
+
+            foreach (var prisoner in filteredPrisoners)
+            {
+                prisoner.ShowInfo();
+            }
+        }
+    }
+
+    enum Crimes
+    {
+        AntiGovernment,
+        DrugTrade,
+        HumanTrafficking,
+        IllegalArmsTrade,
+        Rape,
+        Theft,
+        Murder
+    }
+    
+    class Prisoner
+    {
+        public string Name { get; private set; }
+        public Crimes Crime { get; private set; }
+
+        public Prisoner(string name, Crimes crime)
+        {
+            Name = name;
+            Crime = crime;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Преступник - {Name} | Преступление - {Crime}");
+        }
+    }
+}
